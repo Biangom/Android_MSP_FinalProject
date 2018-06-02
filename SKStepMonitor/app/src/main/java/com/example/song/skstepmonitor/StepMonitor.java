6bd10c614a78ba6b838eb18065b0bf37ccdd1fe0
DESKTOP-K9A4D5V\iMediaLab <zxc940705@naver.com>
2018-06-02 23:19:56 +0900

    first

---
+++ SKStepMonitor/app/src/main/java/com/example/song/skstepmonitor/StepMonitor.java
@@ -0,0 +1,13 @@
+package com.example.song.skstepmonitor;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class StepMonitor extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
